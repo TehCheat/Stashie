26c211dec9ae5fc707c204baed9881583dca4761
TehCheat <thecheatuc@gmail.com>
2021-07-27 00:06:28 -0400

    moved latency reference to ServerData

--- Stashie.cs
+++ Stashie.cs
@@ -792,7 +792,7 @@ namespace Stashie
                 yield break;
             }
 
-            var delay = (int) GameController.Game.IngameState.CurLatency + Settings.ExtraDelay.Value;
+            var delay = (int) GameController.Game.IngameState.ServerData.Latency + Settings.ExtraDelay.Value;
             var currencyTabVisible = false;
             var inventory = GameController.Game.IngameState.IngameUi.InventoryPanel[InventoryIndex.PlayerInventory];
             var stashItems = inventory.VisibleInventoryItems;
@@ -987,7 +987,7 @@ namespace Stashie
 
         private IEnumerator SplitStack(int amount, Vector2 from, Vector2 to)
         {
-            var delay = (int) GameController.Game.IngameState.CurLatency * 2 + Settings.ExtraDelay;
+            var delay = (int)GameController.Game.IngameState.ServerData.Latency * 2 + Settings.ExtraDelay;
             Input.KeyDown(Keys.ShiftKey);
 
             while (!Input.IsKeyDown(Keys.ShiftKey)) yield return new WaitTime(WhileDelay);
